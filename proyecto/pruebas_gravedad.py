002b47498be50c3bf7d3f9287f415e06db7955cd
Oscar Bernal <oebernal@utp.edu.co>
2014-04-27 16:59:23 -0500

    saltos (no infinitos) usando la bandera aire que cambia cuando toca el piso y cuando salta

--- proyecto/pruebas_gravedad.py
+++ proyecto/pruebas_gravedad.py
@@ -19,14 +19,17 @@ class Block(pygame.sprite.Sprite):
 	#Constructor
     def __init__(self, color, width, height):
        pygame.sprite.Sprite.__init__(self)
+       self.width = width
+       self.height = height
        #crea una imagen de las dimenciones espefificadas
-       self.image = pygame.Surface([width, height])
+       self.image = pygame.Surface([self.width, self.height])
        #la rellena con el color correspondiente
        self.image.fill(color)
        #define el rectangulo de coliciones
        self.rect = self.image.get_rect()
        self.vely = 1
        self.velx = 0
+       self.aire = True
 
 ##	FUNCIONES	##
 
@@ -43,6 +46,8 @@ def cargar_imagen(archivo,transpartente = False):
 def gravedad(bloque):
 	bloque.rect.y += bloque.vely
 	bloque.rect.x += bloque.velx
+	if bloque.aire and bloque.vely < 10:
+		bloque.vely += 1
 
 # funcion principal del juego
 def juego():
@@ -60,27 +65,28 @@ def juego():
 	piso = Block(NEGRO,800,100)
 	piso.rect.x=0
 	piso.rect.y=540
-	sprites.add(piso)
 	
 
 	# configuraciones de pygame,reloj y teclado
 	reloj = pygame.time.Clock()
-	pygame.key.set_repeat(1,50)
+	pygame.key.set_repeat(60,0)
 	# ciclo prinicipal
 	while True:
 		#se dibuja el fondo
 		pantalla.blit(fondo,(0,0))
-
 		gravedad(cubo)
 
 		sprites.draw(pantalla)
+		pantalla.blit(piso.image,piso.rect)
 		pygame.display.flip()
 		tecla = pygame.key.get_pressed()
 
 		lista_colicionados_con_piso = pygame.sprite.spritecollide(piso, sprites, False)
 
 		for x in lista_colicionados_con_piso:
-			x.vely = 0;
+			x.aire = False
+			x.vely = 0
+			x.rect.y=piso.rect.y-x.height
 
 		for event in pygame.event.get():
 			if tecla[K_LEFT]:
@@ -88,11 +94,13 @@ def juego():
 			if tecla[K_RIGHT]:
 				cubo.rect.x += 1
 			if tecla[K_UP]:
-				cubo.vely+= -1
+				if not cubo.aire:
+					cubo.vely += -10
+				cubo.aire=True
 			if tecla[K_DOWN]:
 				cubo.vely+= 1
-			if tecla[K_a]:
-				cubo.vely += -3
+			#if tecla[K_a]:
+				
 			if tecla[K_ESCAPE] or event.type == pygame.QUIT:
 				raise SystemExit
 		reloj.tick(60)
