28330f9953cdd74921b7d607acfbebb4638eaeb3
Kenny <breakingwalls84@gmail.com>
2014-06-11 12:27:34 -0500

    se pone un menu inicial de juego, se agregan como nuevos archivos: el fondo del menu, carpeta contenedora de sonidos, carpeta contenedora de fuente del menu inicial, se agrega el sonido de jump al presionar la flecha arriba, y se inicia el juego desde el menu al dar enter en start

--- proyecto/main.py
+++ proyecto/main.py
@@ -83,6 +83,139 @@ def cargar_sprites(archivo,ancho,alto):
 	return tabla_fondos
 
 
+########----------------------------------------------------------------------------
+########---------------Menu de Inicio-----------------------------------------------
+
+class Menu:
+    lista = []
+    pola = []
+    rozmiar_fontu = 32
+    font_path = 'data/coders_crux/coders_crux.ttf'
+    font = pygame.font.Font
+    dest_surface = pygame.Surface
+    ilosc_pol = 0
+    kolor_tla = (51,51,51)
+    kolor_tekstu =  (255, 255, 153)
+    kolor_zaznaczenia = (153,102,255)
+    pozycja_zaznaczenia = 0
+    pozycja_wklejenia = (0,0)
+    menu_width = 0
+    menu_height = 0
+
+    class Pole:
+        tekst = ''
+        pole = pygame.Surface
+        pole_rect = pygame.Rect
+        zaznaczenie_rect = pygame.Rect
+
+    def move_menu(self, top, left):
+        self.pozycja_wklejenia = (top,left) 
+
+    def set_colors(self, text, selection, background):
+        self.kolor_tla = background
+        self.kolor_tekstu =  text
+        self.kolor_zaznaczenia = selection
+        
+    def set_fontsize(self,font_size):
+        self.rozmiar_fontu = font_size
+        
+    def set_font(self, path):
+        self.font_path = path
+        
+    def get_position(self):
+        return self.pozycja_zaznaczenia
+    
+    def init(self, lista, dest_surface):
+        self.lista = lista
+        self.dest_surface = dest_surface
+        self.ilosc_pol = len(self.lista)
+        self.stworz_strukture()        
+        
+    def draw(self,przesun=0):
+        if przesun:
+            self.pozycja_zaznaczenia += przesun 
+            if self.pozycja_zaznaczenia == -1:
+                self.pozycja_zaznaczenia = self.ilosc_pol - 1
+            self.pozycja_zaznaczenia %= self.ilosc_pol
+        menu = pygame.Surface((self.menu_width, self.menu_height))
+        menu.fill(self.kolor_tla)
+        zaznaczenie_rect = self.pola[self.pozycja_zaznaczenia].zaznaczenie_rect
+        pygame.draw.rect(menu,self.kolor_zaznaczenia,zaznaczenie_rect)
+
+        for i in xrange(self.ilosc_pol):
+            menu.blit(self.pola[i].pole,self.pola[i].pole_rect)
+        self.dest_surface.blit(menu,self.pozycja_wklejenia)
+        return self.pozycja_zaznaczenia
+
+    def stworz_strukture(self):
+        przesuniecie = 0
+        self.menu_height = 0
+        self.font = pygame.font.Font(self.font_path, self.rozmiar_fontu)
+        for i in xrange(self.ilosc_pol):
+            self.pola.append(self.Pole())
+            self.pola[i].tekst = self.lista[i]
+            self.pola[i].pole = self.font.render(self.pola[i].tekst, 1, self.kolor_tekstu)
+
+            self.pola[i].pole_rect = self.pola[i].pole.get_rect()
+            przesuniecie = int(self.rozmiar_fontu * 0.2)
+
+            height = self.pola[i].pole_rect.height
+            self.pola[i].pole_rect.left = przesuniecie
+            self.pola[i].pole_rect.top = przesuniecie+(przesuniecie*2+height)*i
+
+            width = self.pola[i].pole_rect.width+przesuniecie*2
+            height = self.pola[i].pole_rect.height+przesuniecie*2            
+            left = self.pola[i].pole_rect.left-przesuniecie
+            top = self.pola[i].pole_rect.top-przesuniecie
+
+            self.pola[i].zaznaczenie_rect = (left,top ,width, height)
+            if width > self.menu_width:
+                    self.menu_width = width
+            self.menu_height += height
+        x = self.dest_surface.get_rect().centerx - self.menu_width / 2
+        y = self.dest_surface.get_rect().centery - self.menu_height / 2
+        mx, my = self.pozycja_wklejenia
+        self.pozycja_wklejenia = (x+mx, y+my) 
+
+########---------------Fin menu inicio----------------------------------------------
+
+######------control menu
+
+def control_menu():
+	#creacion del menu
+    menu= Menu()
+
+    menu.init(['Start','Options','Quit'], ventana)
+    menu.draw()
+
+
+    pygame.key.set_repeat(199,69)
+    pygame.display.update()
+    while 1:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    menu.draw(-1) #here is the Menu class function
+                if event.key == K_DOWN:
+                    menu.draw(1) #here is the Menu class function
+                if event.key == K_RETURN:
+                    if menu.get_position() == 2:#here is the Menu class function
+                        pygame.display.quit()
+                        sys.exit()
+                    if menu.get_position() == 0:
+                        print "hola"
+                        main()                    
+                if event.key == K_ESCAPE:
+                    pygame.display.quit()
+                    sys.exit()
+                pygame.display.update()
+            elif event.type == QUIT:
+                pygame.display.quit()
+                sys.exit()
+        pygame.time.wait(8)
+	
+
+
 #####-----------------------------------------
 #####--------------MAIN
 
@@ -90,8 +223,11 @@ def main():
 	##25x20 en la matriz del ini
 	corrimientox=0
 	corrimientoy=0
-	ventana = crear_ventana(1000,640,"fondo.jpg","mapa1")
+	#ventana = crear_ventana(800,640,"fondo.jpg","mapa1")
 	mapa = cargar_mapa("mapa1.ini","nivel")
+
+	#sonido de saltp
+	sound_jump = pygame.mixer.Sound("sounds/jump.wav")
 	while True:
 		#antes de dibujar los sprite se dibuja el fondo
 		ventana.fill((0,0,0))
@@ -114,11 +250,17 @@ def main():
 				elif event.key == pygame.K_RIGHT:
 					corrimientoy -= 15
 					#carga el nivel 2
-					#mapa = cargar_mapa("mapa1.ini","nivel2")
-					mapa = cargar_mapa("mapa1.ini","nivel")
+					mapa = cargar_mapa("mapa1.ini","nivel2")
+					#mapa = cargar_mapa("mapa1.ini","nivel")
+				if event.key == K_UP:
+					sound_jump.play()
+
 
 ###---ejecucion del main
 if __name__ == "__main__":
-    main()
+    #main()
+    ventana = crear_ventana(800,640,"fondo_menu.png","mapa1")
+    control_menu()
+
 
 
